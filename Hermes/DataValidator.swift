df06cff8310f392066a41a73226be3482c8914ea
bakkertj <tbakker@gmail.com>
2021-03-06 10:56:56 -0600

    Updated validator

--- Hermes/DataValidator.swift
+++ Hermes/DataValidator.swift
@@ -4,5 +4,23 @@
 //
 //  Created by Trevor Bakker on 3/6/21.
 //
+// https://stackoverflow.com/questions/27998409/email-phone-validation-in-swift
 
 import Foundation
+
+func isValidPhoneNumber(value: String) -> Bool {
+    let PHONE_REGEX = "^\\d{3}-\\d{3}-\\d{4}$"
+    let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+    let result = phoneTest.evaluate(with: value)
+    return result
+}
+
+func isValidEmail(testStr:String) -> Bool {
+    print("validate emilId: \(testStr)")
+    let emailRegEx = "^(?:(?:(?:(?: )*(?:(?:(?:\\t| )*\\r\\n)?(?:\\t| )+))+(?: )*)|(?: )+)?(?:(?:(?:[-A-Za-z0-9!#$%&’*+/=?^_'{|}~]+(?:\\.[-A-Za-z0-9!#$%&’*+/=?^_'{|}~]+)*)|(?:\"(?:(?:(?:(?: )*(?:(?:[!#-Z^-~]|\\[|\\])|(?:\\\\(?:\\t|[ -~]))))+(?: )*)|(?: )+)\"))(?:@)(?:(?:(?:[A-Za-z0-9](?:[-A-Za-z0-9]{0,61}[A-Za-z0-9])?)(?:\\.[A-Za-z0-9](?:[-A-Za-z0-9]{0,61}[A-Za-z0-9])?)*)|(?:\\[(?:(?:(?:(?:(?:[0-9]|(?:[1-9][0-9])|(?:1[0-9][0-9])|(?:2[0-4][0-9])|(?:25[0-5]))\\.){3}(?:[0-9]|(?:[1-9][0-9])|(?:1[0-9][0-9])|(?:2[0-4][0-9])|(?:25[0-5]))))|(?:(?:(?: )*[!-Z^-~])*(?: )*)|(?:[Vv][0-9A-Fa-f]+\\.[-A-Za-z0-9._~!$&'()*+,;=:]+))\\])))(?:(?:(?:(?: )*(?:(?:(?:\\t| )*\\r\\n)?(?:\\t| )+))+(?: )*)|(?: )+)?$"
+    let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+    let result = emailTest.evaluate( with: testStr)
+    return result
+}
+
+
